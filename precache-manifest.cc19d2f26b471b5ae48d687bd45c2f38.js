80d9b6a7a3dd260a1e32e4790f19a6961679ad4e
nopparat.m <nopparat.m@yuanta.co.th>
2020-07-01 10:18:01 +0700

    Updates

---
+++ precache-manifest.cc19d2f26b471b5ae48d687bd45c2f38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b46764310c4e25884bff90b117caa4",
+    "url": "/openapp/index.html"
+  },
+  {
+    "revision": "fea47bfe8240c2354f6b",
+    "url": "/openapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab9c6ec3c7f33ccae42c",
+    "url": "/openapp/static/js/2.712cd681.chunk.js"
+  },
+  {
+    "revision": "e4d12dac259e4a26e4d5bcececcff135",
+    "url": "/openapp/static/js/2.712cd681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea47bfe8240c2354f6b",
+    "url": "/openapp/static/js/main.96370e27.chunk.js"
+  },
+  {
+    "revision": "fcf75554c444df50267b",
+    "url": "/openapp/static/js/runtime-main.a144eb2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/openapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
